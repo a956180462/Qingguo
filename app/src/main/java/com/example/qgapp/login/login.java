0bef9d202c2a2064d293714273aeb87b044b3220
c9561 <c956180462@outlook.com>
2022-03-01 18:02:19 +0800

    a956180462

---
+++ app/src/main/java/com/example/qgapp/login/login.java
@@ -0,0 +1,17 @@
+package com.example.qgapp.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class login extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
